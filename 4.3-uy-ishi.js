34f3f028b9ac4cd6bd8b69d4b21a7ccc58fffce2
Akbarof01 <akhbbarov01@gmail.com>
2023-11-09 21:04:41 +0500

    4.3Uy ishi

---
+++ 4.3-uy-ishi.js
@@ -0,0 +1,38 @@
+//(1)
+//let a = "10";
+//let b = 5;
+//let c = +a + b ;
+//console.log(c)
+
+//(2)
+//console.log("besh" * 5) ;
+
+//(3//)
+//let a1;
+//a1 = 10;
+//let temp = a1;
+//a1 = temp;
+//console.log(a1);
+
+// (4)
+//let a2;
+//a2 = 5;
+//console.log(Boolean(a2));
+
+// (5)
+//let a3;
+//a3 = 3;
+//a3++;
+//console.log(a3);
+
+// (6)
+//let a4;
+//a4 = 5;
+//a4--;
+//console.log(a4);
+
+// (7)
+//let a5;
+//a5 = 5;
+//a5 = null;
+//console.log(a5);
